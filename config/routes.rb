5e811125c2dcb0914973a4a6f9f2b282b73ed358
Reejo Samuel <m@reejosamuel.com>
2016-07-20 10:48:36 +0400

    Add user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   post "push", to: 'rooms#push'
-  root to: 'rooms#show'
 
+  authenticate :user do
+    root to: 'rooms#show', as: :authenticated_root
+  end
+
+
+  root to: 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701083531) do
+ActiveRecord::Schema.define(version: 20160719123114) do
 
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,25 @@ ActiveRecord::Schema.define(version: 20160701083531) do
     t.float    "refund"
     t.float    "tip"
     t.float    "void"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "role"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
