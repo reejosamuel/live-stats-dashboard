90d75676891f71acccca1a2b3723f6f655b2a22d
Reejo Samuel <m@reejosamuel.com>
2016-07-24 10:22:59 +0400

    Add First Name and Last Name to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723185937) do
+ActiveRecord::Schema.define(version: 20160724061414) do
 
   create_table "connection_statuses", force: :cascade do |t|
     t.boolean  "status"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20160723185937) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "mobile"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
