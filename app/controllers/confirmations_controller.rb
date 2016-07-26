5c87b7637143d51265249ae3b330bd2987aea6df
Reejo Samuel <m@reejosamuel.com>
2016-07-27 00:03:07 +0400

    More towards changing default login to mobile from email

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,22 +1,5 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
-  # GET /resource/confirmation/new
-  def new
-    self.resource = resource_class.new
-  end
-
-  # POST /resource/confirmation
-  def create
-    self.resource = resource_class.send_confirmation_instructions(resource_params)
-    yield resource if block_given?
-
-    if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
-
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     received_confirmation_token = params[:confirmation_token] || params[:user] && params[:user][:confirmation_token]
